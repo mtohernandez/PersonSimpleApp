4d667e91fc8b6f8d5519244b0dc7d85d09a48123
mtohernandez <m4teoh3rnandez@outlook.com>
2022-05-10 20:29:38 -0500

    Last Update

---
+++ LocationThree/LocationThree/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocationThree
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object obj, EventArgs args)
+        {   
+            string name = entAppName.Text;
+            string lastname = entAppLastName.Text;
+            string age = entAppAge.Text;
+            string address = entAppAddress.Text;
+
+            await Navigation.PushAsync(new SecondPage(name, lastname, age, address));
+
+        }
+    }
+}
---
+++ LocationThree/LocationThree/SecondPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocationThree
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage(string name, string lastname, string age, string address)
+        {
+            InitializeComponent();
+
+            Label namelabel = this.FindByName<Label>("namelbl");
+            Label lastnamelabel = this.FindByName<Label>("lastnamelbl");
+            Label agelabel = this.FindByName<Label>("agelbl");
+            Label addresslabel = this.FindByName<Label>("addresslbl");
+
+            namelabel.Text = name;
+            lastnamelabel.Text = lastname;
+            agelabel.Text = age;
+            addresslabel.Text = address;
+        }
+    }
+}
